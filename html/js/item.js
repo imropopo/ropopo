4b7509c1b5e66bbc2866d26f21ba0724060eeaa7
lingchang00 <lingchang0011@gmail.com>
2024-02-19 15:41:03 +0800

    ropopo
    官網

---
+++ html/js/item.js
@@ -0,0 +1,13 @@
+var clothingContent = [
+    // upper
+	{   
+        "id": "19595",
+		"position": "upper",
+		"itemUrl":'img/clothing/upper/19595-s.png',
+        "wearingUrl":'img/clothing/upper/19595.png',
+		"name": "(服飾)淑女帽",
+		"enchant": "可至綿綿島附魔",
+        "special": "動態",
+        "source": "<li><a href='https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051'>消耗品合成箱</a></li><li><a href='https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051'>消耗品合成箱</a></li>",
+	}
+]
