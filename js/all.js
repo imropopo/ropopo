3dc0f6347f59abd17ac1af244be4235c1a07176a
lingchang00 <lingchang0011@gmail.com>
2024-02-29 15:06:21 +0800

    官網更新0229

--- js/all.js
+++ js/all.js
@@ -1,71 +1,119 @@
 $(function () {
+    const clothingContainer = document.querySelector('.clothingItem');
+    const allClothingItems = Object.values(clothingContent).flat();
+    const navItems = document.querySelectorAll('nav li');
+    let currentFilters = {};
 
-    // ******* 【動態加入服飾資料】 *******
-    // 獲得包含服飾資料的容器
-    let clothingContainer = document.querySelector('.clothingItem');
+    function renderClothingItems(items) {
+        clothingContainer.innerHTML = '';
+        const fragment = document.createDocumentFragment();
+        items.forEach(item => {
+            const figure = createClothingItem(item);
+            fragment.appendChild(figure);
+        });
+        clothingContainer.appendChild(fragment);
+    }
 
-    for (const type in clothingContent) {
-        if (clothingContent.hasOwnProperty(type)) {
-            clothingContent[type].forEach(function (item) {
-                // 建立 figure 元素
-                let figure = document.createElement('figure');
+    function createClothingItem(item) {
+        const figure = document.createElement('figure');
+        figure.innerHTML = `
+            <div class="hamWrapper">
+                <div class="ham">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div class="itemClose">
+                <div class="item">
+                    <img src="img/ArchbishopColor0.png" alt="祭司">
+                    <img class="hair" src="img/Hair7White.png" alt="頭髮">
+                    <img class="hat" src="${item.wearingUrl}" alt="${item.name}">
+                </div>
+                <figcaption>${item.name}${item.enchant ? '<mark></mark>' : ''}</figcaption>
+            </div>
+            <div class="itemOpen">
+                <div class="title ${item.enchant ? 'has-enchant' : ''}">
+                    ${item.name}${item.enchant ? '<mark></mark>' : ''}
+                    <span>${item.enchant}</span>
+                </div>
+                <div class="itemPic">
+                    <img src="${item.itemUrl}">
+                </div>
+                <div class="from">點選出處
+                    ${item.source ? `
+                        <div class="formLink">
+                            ${item.source.map(source => `
+                                <a href="${source.url}" target="_blank">
+                                    <img src="img/iconFrom.svg">
+                                </a>
+                            `).join('')}
+                        </div>` : ''}
+                </div>
+            </div>`;
+        return figure;
+    }
 
-                // 建立內部元素
-                let innerHtml = `
-                    <div class="hamWrapper">
-                        <div class="ham">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                    </div>
-                    <div class="itemClose">
-                        <div class="item">
-                            <img src="img/ArchbishopColor0.png" alt="祭司">
-                            <img class="hair" src="img/Hair7White.png" alt="頭髮">
-                            <img class="hat" src="${item.wearingUrl}" alt="${item.name}">
-                        </div>
-                        <figcaption>${item.name}<mark></mark></figcaption>
-                    </div>
-                    <div class="itemOpen">
-                        <div class="title">
-                            ${item.name}<mark></mark>
-                            <span>${item.enchant}</span>
-                        </div>
-                        <div class="itemPic">
-                            <img src="${item.itemUrl}">
-                        </div>
-                        <div class="from">點選出處
-                        ${
-                            item.source ? `
-                                <div class="formLink">
-                                    <a href="${item.source}" target="_blank">
-                                        <img src="img/iconFrom.svg">
-                                    </a>
-                                </div>
-                            ` : ''
-                        }
-                        </div>
-                    </div>`;
-
-                figure.innerHTML = innerHtml;
-
-                // 將 figure 元素加入容器中
-                clothingContainer.appendChild(figure);
-
-                // ******* 【漢堡選單】 *******
-                // 取得剛剛添加的 .hamWrapper 元素
-                let hamWrapper = figure.querySelector('.hamWrapper');
-
-                // 建立監聽事件，使漢堡個別作用
-                hamWrapper.addEventListener('click', function () {
-                    /// 在當下的 figure中找.ham, .title, .item, .from, .itemPic, figcaption元素 切換
-                    let relatedElements = figure.querySelectorAll('.hamWrapper, .ham, .title, .item, .from, .itemPic, figcaption');
-                    relatedElements.forEach(function (element) {
-                        element.classList.toggle('is-active');
-                    });
-                });
+    // 使用事件委托来处理 .hamWrapper 元素的点击事件
+    clothingContainer.addEventListener('click', function (event) {
+        const hamWrapper = event.target.closest('.hamWrapper');
+        if (hamWrapper) {
+            const relatedElements = hamWrapper.parentElement.querySelectorAll('.hamWrapper, .ham, .title, .item, .from, .itemPic, figcaption');
+            relatedElements.forEach(element => {
+                element.classList.toggle('is-active');
             });
         }
+    });
+
+    // 点击导航项时更新筛选条件并重新渲染服装项目
+    navItems.forEach(item => {
+        item.addEventListener('click', function () {
+            this.classList.toggle('selected');
+            currentFilters = updateFilters();
+            const filteredItems = allClothingItems.filter(item => filterItem(item, currentFilters));
+            renderClothingItems(filteredItems);
+        });
+    });
+
+    // 过滤单个项目
+    function filterItem(item, filters) {
+        for (const key in filters) {
+            if (filters.hasOwnProperty(key) && filters[key] !== 'all') {
+                if (key === 'position') {
+                    if (!filters[key].includes(item[key])) {
+                        return false;
+                    }
+                } else if (key === 'enchant' && item[key] === '') {
+                    return false;
+                } else if (filters[key] !== item[key]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
+
+    // 更新筛选条件
+    function updateFilters() {
+        const filters = {};
+        navItems.forEach(item => {
+            if (item.classList.contains('selected')) {
+                const position = item.getAttribute('data-position');
+                const enchant = item.getAttribute('data-enchant');
+                if (position && position !== 'all') {
+                    filters['position'] = (filters['position'] || []).concat(position);
+                }
+                if (enchant && enchant === 'enchant') {
+                    const nonEmptyEnchantItems = allClothingItems.filter(item => item['enchant'] !== '');
+                    if (nonEmptyEnchantItems.length > 0) {
+                        filters['enchant'] = 'enchant';
+                    }
+                }
+            }
+        });
+        return filters;
+    }
+
+    // 初始渲染所有服装项目
+    renderClothingItems(allClothingItems.filter(item => filterItem(item, currentFilters)));
 });
--- js/item.js
+++ js/item.js
@@ -3,8 +3,8 @@ let clothingContent = {
 		{
 			"id": "19595",
 			"position": "upper",
-			"itemUrl": '../img/clothing/upper/19595-s.png',
-			"wearingUrl": '../img/clothing/upper/19595.png',
+			"itemUrl": 'img/clothing/upper/19595-s.png',
+			"wearingUrl": 'img/clothing/upper/19595.png',
 			"name": "(服飾)淑女帽",
 			"enchant": "可附魔",
 			"special": "",
@@ -13,37 +13,303 @@ let clothingContent = {
 		{
 			"id": "19649",
 			"position": "upper",
-			"itemUrl": '../img/clothing/upper/19649-s.png',
-			"wearingUrl": '../img/clothing/upper/19649.png',
+			"itemUrl": 'img/clothing/upper/19649-s.png',
+			"wearingUrl": 'img/clothing/upper/19649.png',
 			"name": "(服飾)白色小貓咪耳飾",
 			"enchant": "可附魔",
 			"special": "",
-			"source": "https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051"
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
 		},
 		{
 			"id": "19915",
 			"position": "upper",
-			"itemUrl": '../img/clothing/upper/19915-s.png',
-			"wearingUrl": '../img/clothing/upper/19915.png',
+			"itemUrl": 'img/clothing/upper/19915-s.png',
+			"wearingUrl": 'img/clothing/upper/19915.png',
 			"name": "(服飾)惡魔小獸角",
-			"enchant": "不可附魔",
-			"special": "動態",
-			"source": " [消耗品合成箱](https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051)"
+			"enchant": "",
+			"special": "",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
 		},
 		{
 			"id": "19903",
 			"position": "upper",
-			"itemUrl": '../img/clothing/upper/19915-s.png',
-			"wearingUrl": '../img/clothing/upper/19915.png',
+			"itemUrl": 'img/clothing/upper/19903-s.png',
+			"wearingUrl": 'img/clothing/upper/19903.png',
 			"name": "(服飾)魔女帽",
-			"enchant": "不可附魔",
+			"enchant": "",
 			"special": "動態",
-			"source": " [消耗品合成箱](https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051)"
-		}
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+		{
+			"id": "19685",
+			"position": "upper",
+			"itemUrl": 'img/clothing/upper/19685-s.png',
+			"wearingUrl": 'img/clothing/upper/19685.png',
+			"name": "(服飾)閃亮的聖誕波利帽子",
+			"enchant": "",
+			"special": "動態",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=443",
+					"note": "影子王者轉蛋"
+				},{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+		{
+			"id": "19695",
+			"position": "upper",
+			"itemUrl": 'img/clothing/upper/19695-s.png',
+			"wearingUrl": 'img/clothing/upper/19695.png',
+			"name": "(服飾)魔法水壺",
+			"enchant": "",
+			"special": "",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+		{
+			"id": "19724",
+			"position": "upper",
+			"itemUrl": 'img/clothing/upper/19724-s.png',
+			"wearingUrl": 'img/clothing/upper/19724.png',
+			"name": "(服飾)狄波蘿太人偶帽",
+			"enchant": "可附魔",
+			"special": "",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+		{
+			"id": "19696",
+			"position": "upper",
+			"itemUrl": 'img/clothing/upper/19696-s.png',
+			"wearingUrl": 'img/clothing/upper/19696.png',
+			"name": "(服飾)哥倆好帽子",
+			"enchant": "可附魔",
+			"special": "動態",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+		{
+			"id": "19718",
+			"position": "upper",
+			"itemUrl": 'img/clothing/upper/19718-s.png',
+			"wearingUrl": 'img/clothing/upper/19718.png',
+			"name": "(服飾)哥德式髮圈",
+			"enchant": "",
+			"special": "",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+		{
+			"id": "19997",
+			"position": "upper",
+			"itemUrl": 'img/clothing/upper/19997-s.png',
+			"wearingUrl": 'img/clothing/upper/19997.png',
+			"name": "(服飾)射彈帽",
+			"enchant": "可附魔",
+			"special": "",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=3854",
+					"note": "服飾系列合成箱"
+				},{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+		{
+			"id": "19998",
+			"position": "upper",
+			"itemUrl": 'img/clothing/upper/19998-s.png',
+			"wearingUrl": 'img/clothing/upper/19998.png',
+			"name": "(服飾)龍龜帽",
+			"enchant": "",
+			"special": "",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+		{
+			"id": "19760",
+			"position": "upper",
+			"itemUrl": 'img/clothing/upper/19760-s.png',
+			"wearingUrl": 'img/clothing/upper/19760.png',
+			"name": "(服飾)七彩面紗",
+			"enchant": "可附魔",
+			"special": "",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=3854",
+					"note": "服飾系列合成箱"
+				},
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+		{
+			"id": "19751",
+			"position": "upper",
+			"itemUrl": 'img/clothing/upper/19751-s.png',
+			"wearingUrl": 'img/clothing/upper/19751.png',
+			"name": "(服飾)光明與黑暗王冠",
+			"enchant": "",
+			"special": "",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=3854",
+					"note": "服飾系列合成箱"
+				},
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+		{
+			"id": "19761",
+			"position": "upper",
+			"itemUrl": 'img/clothing/upper/19761-s.png',
+			"wearingUrl": 'img/clothing/upper/19761.png',
+			"name": "(服飾)白百合花帽",
+			"enchant": "可附魔",
+			"special": "",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=3854",
+					"note": "服飾系列合成箱"
+				},
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
 	],
-	mid:[],
-	mid_upper:[],
-	shawl:[],
-	upper_mid_bottom:[],
+
+	mid:[
+		{
+			"id": "19903",
+			"position": "mid",
+			"itemUrl": 'img/clothing/upper/19903-s.png',
+			"wearingUrl": 'img/clothing/upper/19903.png',
+			"name": "(服飾)魔女帽",
+			"enchant": "",
+			"special": "動態",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+		{
+			"id": "19903",
+			"position": "mid",
+			"itemUrl": 'img/clothing/upper/19903-s.png',
+			"wearingUrl": 'img/clothing/upper/19903.png',
+			"name": "(服飾)魔女帽",
+			"enchant": "",
+			"special": "動態",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+	],	
+
 	bottom:[],
-}
+
+	twoPosition:[
+		{
+			"id": "19903",
+			"position": "twoPosi",
+			"itemUrl": 'img/clothing/upper/19903-s.png',
+			"wearingUrl": 'img/clothing/upper/19903.png',
+			"name": "(服飾)魔女帽",
+			"enchant": "",
+			"special": "動態",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+	],
+
+	threePosition:[
+		{
+			"id": "19903",
+			"position": "threePosi",
+			"itemUrl": 'img/clothing/upper/19903-s.png',
+			"wearingUrl": 'img/clothing/upper/19903.png',
+			"name": "(服飾)魔女帽",
+			"enchant": "",
+			"special": "動態",
+			"source":[
+				{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},{
+					"url":"https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=216051",
+					"note": "消耗品合成箱"
+				},
+			] 
+		},
+	],
+
+	shawl:[]
+}	
